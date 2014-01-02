87a1f158ced54e9ce7dc954fd91a320e80906d58
Kyle Manna <kyle@kylemanna.com>
2014-01-01 23:42:11 -0800

    WIP: Add Yubikey driver singleton

---
+++ src/keys/drivers/Yubikey.h
@@ -0,0 +1,61 @@
+/*
+*  Copyright (C) 2014 Kyle Manna <kyle@kylemanna.com>
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 2 or (at your option)
+*  version 3 of the License.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef KEEPASSX_YUBIKEY_H
+#define KEEPASSX_YUBIKEY_H
+
+#include <QObject>
+
+#include <yubikey.h>
+#include <ykcore.h>
+#include <ykdef.h>
+#include <ykstatus.h>
+#include <ykpers-version.h>
+
+/**
+ * Singleton class to manage the interface to the hardware
+ */
+class Yubikey
+{
+public:
+    enum ChallengeResult { ERROR = -1, SUCCESS = 0, WOULDBLOCK };
+
+    static Yubikey* instance();
+
+    bool init();
+
+    ChallengeResult challenge(int slot, bool mayBlock,
+                              const QByteArray& chal,
+                              QByteArray& resp) const;
+
+    unsigned int getSerial() const;
+
+private:
+    explicit Yubikey();
+
+    static Yubikey* m_instance;
+    YK_KEY *m_yk;
+    unsigned int m_serial;
+
+    Q_DISABLE_COPY(Yubikey)
+};
+
+inline Yubikey* yubikey() {
+    return Yubikey::instance();
+}
+
+#endif // KEEPASSX_YUBIKEY_H
