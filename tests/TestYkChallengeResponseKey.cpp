c168d3505342dd03dcc88796786b254f2ac28062
Kyle Manna <kyle@kylemanna.com>
2014-05-26 00:08:10 -0700

    tests: Rename unit tests to match parent class
    * Matching is sharing.  Sharing is caring.

--- tests/TestYkChallengeResponseKey.cpp
+++ tests/TestYkChallengeResponseKey.cpp
@@ -16,7 +16,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "TestYubiKeyChalResp.h"
+#include "TestYkChallengeResponseKey.h"
 
 #include <QTest>
 #include <QtConcurrentRun>
